41c5541190751bcc0064cbb246dd07e0ffc60962
LucyQ19 <lucyq19@gmail.com>
2019-05-25 22:10:37 -0500

    added contents to app.js

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+    $("#burgerForm").on("submit", function(events) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#newBurger").val().trim(),
+            devoured: 0
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(function(){
+            console.log("Added new Burger!");
+
+            location.reload();
+        });
+    });
+
+    $("#eatBtn").on("click", function(event){
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        var devouredState = {
+            devoured: 1
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(function() {
+            console.log("Burger devoured!");
+
+            location.reload();
+        });
+    });
+
+    $("#devourBtn").on("click", function(event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+
+        $.ajax({
+            type: "DELETE",
+            url: "/api/burgers/" + id
+        }).then(location.reload());
+    });
+});
